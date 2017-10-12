0ed3fbb4c2068c7cde0b1d8fbee9ac6c1b71fb3d
Jonathan Soo <jcsoo@agora.com>
2017-10-12 12:04:12 -0400

    Remove dead code, and clean up

--- p1.py
+++ p1.py
@@ -14,7 +14,7 @@ DEBUG = False
 CANNY_LOW = 50
 CANNY_HIGH = 255
 
-GAUSS_KERNEL = 3
+GAUSS_KERNEL = 0
 
 # fractional width of parallelogram top
 ROI_TOP_WIDTH = 0.30
@@ -32,28 +32,40 @@ HOUGH_MIN_LINE_LEN = 10
 # maximum gap in pixels between connectable line segments
 HOUGH_MAX_LINE_GAP = 5
 
-LINE_COLOR = [0, 255, 0]
-LINE_WIDTH = 4
+# Line Grouping
 
 GROUP_ENABLE = True
 
+# Grouping Parameters
+
+# Amount of rounding for line offsets
 GROUP_OFFSET = 1000.0
+# Amount of rounding for slopes
 GROUP_SLOPE = 0.5
+# Minimum number of lines in a group
 GROUP_MIN_NUM = 1
+# Minimum total length of lines in a group
 GROUP_MIN_SUM = 1
 
+# Enable filtering lines by offset + slope envelope
 GROUP_ITEM_ENABLE_ENVELOPE = True
-GROUP_ENABLE_ENVELOPE = False
 
-GROUP_LEFT_OFFSET_MIN = -0.2
-GROUP_LEFT_OFFSET_MAX = 0.1
-GROUP_LEFT_SLOPE_MIN = 1.0
-GROUP_LEFT_SLOPE_MAX = 2.0
+# Offset + Slope envelope parameters
+GROUP_RIGHT_OFFSET_MIN = -0.2
+GROUP_RIGHT_OFFSET_MAX = 0.1
+GROUP_RIGHT_SLOPE_MIN = 1.0
+GROUP_RIGHT_SLOPE_MAX = 2.0
 
-GROUP_RIGHT_OFFSET_MIN = 0.8
-GROUP_RIGHT_OFFSET_MAX = 1.1
-GROUP_RIGHT_SLOPE_MIN = -2.0
-GROUP_RIGHT_SLOPE_MAX = -1.2
+# Offset + Slope envelope parameters
+GROUP_LEFT_OFFSET_MIN = 0.8
+GROUP_LEFT_OFFSET_MAX = 1.1
+GROUP_LEFT_SLOPE_MIN = -2.0
+GROUP_LEFT_SLOPE_MAX = -1.2
+
+# Line Drawing
+
+LINE_COLOR = [0, 255, 0]
+LINE_WIDTH = 4
 
 
 # multiplier for lines
@@ -77,7 +89,9 @@ def image_roi(img, top_width, top_height):
 def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     return cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
 
-def len_slope_intercept(line):
+def len_slope_offset(line):
+    # Calculate the length, slope and offset of the line.
+    # NOTE: actually using the reciprocal of the slope because most of our lines will be  close to vertical
     si = []
     for x1, y1, x2, y2 in line:
         l = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
@@ -86,17 +100,17 @@ def len_slope_intercept(line):
         return (l, p[0], p[1])   
 
 
-def filter_lines(lines, vertices):
+def group_lines(lines, vertices):
     # Basic Approach
     #   Convert lines into (slope, intercept, length) form
-    #   Bin into (slope, intercept)
-    #   Search grid for boxes with sum(length) above threshold
-    #     - Weighted average (slope, intercept) => line    
-    #     - Linear regression
+    #   NOTE: actually using the reciprocal of the slope because most of our lines will be  close to vertical
+    #   Bin into (slope, intercept) groups, 
+    #   for each bin
+    #       filter for items within envelope
+    #       length-weighted average of slope, intercept
+    #       generate line bound by ROI
 
-    # TODO - group based on positive / negative slope, linear regression of each
-    # TODO - draw line based on slope + intercept
-    # TODO - return line list with line type (LINE_SOLID, LINE_DASHED, LINE_LEFT, LINE_RIGHT, LINE_WHITE, LINE_YELLOW)
+    # Calculate bounds from ROI vertices
 
     y_max = vertices[0][0][1]
     y_min = vertices[0][1][1]
@@ -105,126 +119,79 @@ def filter_lines(lines, vertices):
     x_max = vertices[0][3][0]
     x_mid = int((x_max + x_min) / 2)
 
+    # Add all lines to dictionary using rounded slope and offset as the key. GROUP_SLOPE and GROUP_OFFSET
+    # control the amount of rounding.
+
     clusters = {}
 
     for line in lines:
-        (l, s, i) = len_slope_intercept(line)
-        key = (round(s * GROUP_SLOPE) / GROUP_SLOPE, round(i / GROUP_OFFSET) * GROUP_OFFSET)
-        #print(line, l, s, i, key)
+        (l, s, o) = len_slope_offset(line)
+        key = (round(s * GROUP_SLOPE) / GROUP_SLOPE, round(o / GROUP_OFFSET) * GROUP_OFFSET)
         v = clusters.get(key, [])
-        v.append((line, l, s, i))
+        v.append((line, l, s, o))
         clusters[key] = v
 
     keys = list(clusters.keys())
     keys.sort()
 
-    #print("groups:", len(keys))
-
     out = []
+
+    # For each group of lines in the cluster, reject lines outside of envelope and then
+    # perform a length-weighted average of slope + offset.
 
     for k in keys:
         v = clusters[k]
         v_lines = [l[0] for l in v]
         num = len(v)
         sum_len = sum([l[1] for l in v])
-        #print(k, len(v), sum_len)
         if num >= GROUP_MIN_NUM and sum_len >= GROUP_MIN_SUM:
-            #print(k)
             s_sum = 0.0
-            i_sum = 0.0
+            o_sum = 0.0
             l_sum = 0.0
-            # y_min = 0.0
-            # y_max = 0.0
             for item in v:
-                (line, l, s, i) = item
-                #print("  %s %4.2f %f %f" % (line, l, s, i))
+                (line, l, s, o) = item
                 if GROUP_ITEM_ENABLE_ENVELOPE:
-                    if i > x_mid:
-                        if i < x_max * GROUP_RIGHT_OFFSET_MIN or i > x_max * GROUP_RIGHT_OFFSET_MAX:
-                            #print('    right reject offset', s_avg, i_avg, sum_len)
-                            continue
-                        if s < GROUP_RIGHT_SLOPE_MIN or s > GROUP_RIGHT_SLOPE_MAX:
-                            if DEBUG:
-                                print('    right reject slope', s, i, l)
-                            continue
-                    else:
-                        # print('  left')
-                        if i < x_max * GROUP_LEFT_OFFSET_MIN or i > x_max * GROUP_LEFT_OFFSET_MAX:
+                    if o > x_mid:
+                        if o < x_max * GROUP_LEFT_OFFSET_MIN or o > x_max * GROUP_LEFT_OFFSET_MAX:
                             #print('    left reject offset', s_avg, i_avg, sum_len)
                             continue
                         if s < GROUP_LEFT_SLOPE_MIN or s > GROUP_LEFT_SLOPE_MAX:
                             if DEBUG:
-                                print('    left reject slope', s, i, l)
+                                print('    left reject slope', s, o, l)
+                            continue
+                    else:
+                        # print('  left')
+                        if o < x_max * GROUP_RIGHT_OFFSET_MIN or o > x_max * GROUP_RIGHT_OFFSET_MAX:
+                            #print('    right reject offset', s_avg, i_avg, sum_len)
+                            continue
+                        if s < GROUP_RIGHT_SLOPE_MIN or s > GROUP_RIGHT_SLOPE_MAX:
+                            if DEBUG:
+                                print('    right reject slope', s, o, l)
                             continue
 
 
                 s_sum += (l * s)
-                i_sum += (l * i)
+                o_sum += (l * o)
                 l_sum += l
-                # (x1, y1, x2, y2) = line[0]
 
-                # dx = x2 - x1
-                # dy = y2 - y1
-                # m = dx / dy
-
-                #print("      dx = %f dy = %f m = %f" % (dx, dy, m))
-
-                # if y1 < y_min or y_min == 0.0:
-                #     y_min = y1
-                # if y2 < y_min:
-                #     y_min = y2
-                # if y1 > y_max:
-                #     y_max = y1
-                # if y2 > y_max:
-                #     y_max = y2
-
+            # No items left in this group due to filtering
             if l_sum == 0:
                 continue
 
-
-
             s_avg = s_sum / l_sum
-            i_avg = i_sum / l_sum
-            #print("  %f %f (%f %f)" % (s_avg, i_avg, y_min, y_max))
-            
-            # filter for sane s_avg
+            o_avg = o_sum / l_sum
 
-            if GROUP_ENABLE_ENVELOPE:
-                if i_avg > x_mid:
-                    if i_avg < x_max * GROUP_RIGHT_OFFSET_MIN or i_avg > x_max * GROUP_RIGHT_OFFSET_MAX:
-                        #print('    right reject offset', s_avg, i_avg, sum_len)
-                        continue
-                    if s_avg < GROUP_RIGHT_SLOPE_MIN or s_avg > GROUP_RIGHT_SLOPE_MAX:
-                        print('    right reject slope', s_avg, i_avg, sum_len)
-                        continue
-                else:
-                    # print('  left')
-                    if i_avg < x_max * GROUP_LEFT_OFFSET_MIN or i_avg > x_max * GROUP_LEFT_OFFSET_MAX:
-                        #print('    left reject offset', s_avg, i_avg, sum_len)
-                        continue
-                    if s_avg < GROUP_LEFT_SLOPE_MIN or s_avg > GROUP_LEFT_SLOPE_MAX:
-                        print('    left reject slope', s_avg, i_avg, sum_len)
-                        continue
-
-
-            # y = mx + b
-
-            line = v[0][0][0]            
-            
-            # y0 = int(line[1])
-            # x0 = int((y0 * s_avg) + i_avg)
-            # y1 = int(line[3])
-            # x1 = int((y1 * s_avg) + i_avg)
+            # Calculate the line from y_min to y_max
 
             y0 = int(y_min)
-            x0 = int((y0 * s_avg) + i_avg)
+            x0 = int((y0 * s_avg) + o_avg)
             y1 = int(y_max)
-            x1 = int((y1 * s_avg) + i_avg)
+            x1 = int((y1 * s_avg) + o_avg)
 
             line = [[x0, y0, x1, y1]]
-            #print(" line:", line)
+
+            # Add it to the output
             out.append(line)   
-            #out.extend(v_lines)
     return out
 
 def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
@@ -234,22 +201,37 @@ def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
             cv2.line(line_img, (x1, y1), (x2, y2), color, thickness)
     return line_img
 
-def process_image(img):    
+def process_image(img):
+    # Keep the original image
     img_orig = img.copy()
-    vertices = image_roi(img, ROI_TOP_WIDTH, ROI_TOP_HEIGHT)
-    #print(vertices)
-    img = helper.grayscale(img)
-    img = helper.canny(img, CANNY_LOW, CANNY_HIGH)
-    # img = helper.gaussian_blur(img, GAUSS_KERNEL)     
-    
-    img = helper.region_of_interest(img, vertices)    
-    #return img
-    lines = hough_lines(img, HOUGH_RHO, HOUGH_THETA, HOUGH_THRESHOLD, HOUGH_MIN_LINE_LEN, HOUGH_MAX_LINE_GAP)    
-    if GROUP_ENABLE:
-        lines = filter_lines(lines, vertices)
 
+    # Calculate the vertices based on the image size
+    vertices = image_roi(img, ROI_TOP_WIDTH, ROI_TOP_HEIGHT)
+    
+    # Convert to grayscale
+    img = helper.grayscale(img)
+
+    # Run the Canny edge filter
+    img = helper.canny(img, CANNY_LOW, CANNY_HIGH)
+
+    if GAUSS_KERNEL > 0:
+        # Blur the edge output
+        img = helper.gaussian_blur(img, GAUSS_KERNEL)     
+    
+    # Mask the region of interest
+    img = helper.region_of_interest(img, vertices)    
+    
+    # Generate candidate lines using the Hough transform
+    lines = hough_lines(img, HOUGH_RHO, HOUGH_THETA, HOUGH_THRESHOLD, HOUGH_MIN_LINE_LEN, HOUGH_MAX_LINE_GAP)    
+
+    if GROUP_ENABLE:        
+        # Group the candidate lines
+        lines = group_lines(lines, vertices)
+
+    # Draw final lines
     img_lines = draw_lines(img_orig, lines, LINE_COLOR, LINE_WIDTH)
     
+    # Merge the original image with the generated lines.
     return helper.weighted_img(img_orig, img_lines, MERGE_ALPHA, MERGE_BETA, MERGE_LAMBDA)
 
 def main(args):
